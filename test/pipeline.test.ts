89b7d12a6f55c7caeaa063d4adaddbc84a249e7f
RunEagler <ohk.cotf1649@gmail.com>
2022-06-10 00:07:19 +0900

    update: snapshot test

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -1,17 +1,11 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Pipeline from '../lib/pipeline-stack';
+import * as cdk from 'aws-cdk-lib'
+import { Template } from 'aws-cdk-lib/assertions'
+import * as Pipeline from '../lib/pipeline-stack'
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/pipeline-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Pipeline.PipelineStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
-});
+test('Pipeline Stack', () => {
+  const app = new cdk.App()
+  // WHEN
+  const stack = new Pipeline.PipelineStack(app, 'MyTestStack')
+  const template = Template.fromStack(stack)
+  expect(template.toJSON()).toMatchSnapshot()
+})
